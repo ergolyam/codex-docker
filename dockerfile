e4d3b9e36440805e57ddff5692a73faa61f78b3f
ergolyam <grisha765@tuta.io>
2026-01-06 19:08:43 +0300

    [+] init project

---
+++ dockerfile
@@ -0,0 +1,21 @@
+ARG DIST=alpine
+ARG RUST_IMAGE=rust:alpine
+ARG BASE_IMAGE=alpine:latest
+
+FROM docker.io/${RUST_IMAGE} AS builder
+
+COPY install-deps.sh ./install-deps.sh
+RUN ./install-deps.sh ${DIST}
+
+RUN git clone https://github.com/openai/codex -b rust-v0.77.0 /build
+
+WORKDIR /build
+
+RUN cargo build --manifest-path=codex-rs/Cargo.toml --release
+
+
+FROM docker.io/${BASE_IMAGE} as main
+
+COPY --from=builder /build/codex-rs/target/release/codex /usr/bin
+
+CMD [ "codex" ]
---
+++ install-deps.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+DISTRO="$(printf '%s' "$1" | tr '[:upper:]' '[:lower:]')"
+
+case "${DISTRO}" in
+    alpine)
+      apk add --no-cache \
+        build-base \
+        git \
+        perl \
+        openssl-dev \
+        ca-certificates || exit 1
+      ;;
+    debian)
+      apt-get update -y
+      apt-get install -y --no-install-recommends \
+        build-essential \
+        git \
+        perl \
+        libssl-dev \
+        ca-certificates || exit 1
+      rm -rf /var/lib/apt/lists/*
+      ;;
+esac
+
+echo "Cleaning up: removing $0"
+rm -f -- "$0"
